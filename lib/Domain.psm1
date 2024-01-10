cc81d03d95f1922be13a95d2a88703e2dfaebc7c
cuiyanx <yanx.cui@intel.com>
2024-01-10 15:38:17 +0800

    Update: delete VHD file for LiveMigration

--- lib/Domain.psm1
+++ lib/Domain.psm1
@@ -335,10 +335,15 @@ function Domain-RemoveVMs
                         }
 
                         Remove-VM -Name $VM.Name -Force -Confirm:$false | out-null
-                        if ($VM.HardDrives.Path -match "QatServer305") {
-                            $VMArray += $VM.HardDrives.Path
-                        } else {
-                            Remove-Item -Path $VM.HardDrives.Path -Force -Confirm:$false | out-null
+                        if (-not [String]::IsNullOrEmpty($VM.HardDrives.Path)) {
+                            $KeyWords = $LocationInfo.Domain.S2DStorage.split("\\")[1]
+                            if ($VM.HardDrives.Path -match $KeyWords) {
+                                $VMArray += $VM.HardDrives.Path
+                            } else {
+                                if (Test-Path -Path $VM.HardDrives.Path) {
+                                    Remove-Item -Path $VM.HardDrives.Path -Force -Confirm:$false | out-null
+                                }
+                            }
                         }
                     }
                 }
